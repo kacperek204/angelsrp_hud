c7bd6111ebd2934d1c1053656e23025894d6bd01
kacperek204 <71221171+kacperek204@users.noreply.github.com>
2020-09-14 17:31:00 +0200

    angelsrp_hud
    angelsrp_hud

---
+++ config.lua
@@ -0,0 +1,70 @@
+Config = {}
+
+Config.Locale = 'en'
+
+Config.serverLogo = 'https://i.imgur.com/VOTPwi1.png'
+
+Config.font = {
+	name 	= 'Montserrat',
+	url 	= 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700,900&display=swap'
+}
+
+Config.date = {
+	format	 	= 'withHours',
+	AmPm		= false
+}
+
+Config.voice = {
+
+	levels = {
+		default = 12.0,
+		shout = 19.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= 'F10',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KM/H',
+	maxSpeed = 400,
+
+	keys = {
+		seatbelt 	= 'L',
+		cruiser		= 'CAPS',
+		signalLeft	= '"',
+		signalRight	= '"',
+		signalBoth	= '"',
+	}
+}
+
+Config.ui = {
+	showServerLogo		= false,
+
+	showJob		 		= true,
+
+	showWalletMoney 	= true,
+	showBankMoney 		= true,
+	showBlackMoney 		= true,
+	showSocietyMoney	= true,
+
+	showDate 			= false,
+	showLocation 		= false,
+	showVoice	 		= true,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+	showSranje	 		= false,
+	showPisanje	 		= false,
+
+	showMinimap			= false,
+
+	showWeapons			= false,	
+}
